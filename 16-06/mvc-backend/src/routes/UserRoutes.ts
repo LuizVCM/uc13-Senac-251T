10209e43564bb9ab11ec22debeeec8c997313af9
LuizVCM <luizvcmluizvcm@gmail.com>
2026-06-17 17:12:19 -0300

    feat: adiciona o resto

---
+++ 16-06/mvc-backend/src/routes/UserRoutes.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { UsuarioController } from "../controllers/UserController";
+
+const router = Router()
+ const controller = new UsuarioController()
+
+router.get("/usuarios", (req, res) => controller.listUsuarios(req, res))
+router.get("/usuarios/:id", (req, res) => controller.getUsuario(req, res))
+router.post("/usuarios", (req, res) => controller.createUsuario(req, res))
+router.put("/usuarios/:id", (req, res) => controller.updateUsuario(req, res))
+router.delete("/usuarios/:id", (req, res) => controller.deleteUsuario(req, res))
+
+export default router
--- 16-06/mvc-backend/src/services/UserService.ts
+++ 16-06/mvc-backend/src/services/UserService.ts
@@ -16,7 +16,31 @@ export class UserService {
        const [result] = await db.query(
         "INSERT INTO usuarios (email, password) VALUES (?, ?)", [user.getEmail(), user.getSenha()]
        )
+       return result;
 
-
+  }
+  async findAll(){
+    const [rows] = await db.query(
+        "SELECT * FROM usuarios"
+    )
+    return rows 
   } 
+  async findById(id:number){
+    const [rows]: any = await db.query(
+      "SELECT * FROM usuarios WHERE id = ?", [id]
+    )
+    return rows[0]
+  }
+  async update(id: number, email: string, password: string){
+       const [result] = await db.query(
+        `UPDATE usuarios SET email = ?, password = ? WHERE id = ?`, [email, password, id]
+       )
+       return result
+  }
+  async delete(id:number){
+   const [result] = await db.query(
+    "DELET FROM usuarios WHERE id = ?", [id]
+   )
+   return result
+  }
 }
