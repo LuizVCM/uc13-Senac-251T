fe050fb0c0ddae6579d1e333a192a4a6eab183cc
LUIZ VINICIUS CASSABONE MUNIZ <04527409085@senacrs.edu.br>
2026-06-30 16:39:45 -0300

    adiciona 30-06

---
+++ 30-06/src/models/User.ts
@@ -0,0 +1,34 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn,  } from "typeorm";
+import { Post } from "./Post";
+
+// Precisamos sinalizar ao TypeORM que esta classe será mapeada em uma tabela
+// Para fazer isso, utilizamos um decorator:
+// @Entity('users) indica qur deve ser criada uma tabela com o nome 'users'
+@Entity('users')
+export class User{
+    // PrimaryGeneratedCollumn() indica que este atributo será uma coluna com PRIMARY KEY e AUTO_INCREMENT
+    @PrimaryGeneratedColumn()
+    id:number;
+   
+    // @Column marca o atributo com uma coluna 'normal'
+    // length diz qual o tamanho máximo de caracteres
+    // nullable:false diz que não pode ser nula (tipo o NOT NULL)
+    @Column({length:150, nullable:false})
+    name:string;
+
+    @Column({length:150, nullable:false, unique:true})
+    email:string;
+
+    @Column({select: false, nullable:false})
+    password:string;
+
+    // @OneToMany indica que um User pode ter vários Post
+    // precisamos passar dois parâmetros:
+    // () => Post -> função que retorna a entidade relacionada
+    // post => post.user -> indica qual a propriedade na classe Post que referencia o User
+    // Com tudo isso definido, o TypeORM consegue criart automaticamente as ligações entre as tabelas e as chaves estrangeiras.
+    // Temos que fazer sem,pre para todos ops envolvidos, neste caso, tanto para User quanto para Post
+    @OneToMany(() => Post, post => post.user)
+    posts:Post[] 
+}
+
---
+++ 30-06/src/server.ts
@@ -0,0 +1,18 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+import { AppDataSource } from './config/data-source'
+
+const app = express()
+dotenv.config()
+const PORT = process.env.PORT // Pega o valor da variável PORT que está no .env
+
+// initialize() é um método do TypeORM qyue abre a conexão com o banco usando as configurações que escrevemos no data-source. Ele também carrega as entidades e executa a criação das tabelas.
+// then() -> a função dentro dele é executada se der certo
+// catch() -> a função dentro dele roda se houver erro
+AppDataSource.initialize().then(() => {
+    console.log("Banco conectado com sucesso!!!!!!!!!")
+
+    app.listen(PORT, () => {
+        console.log("servidor backend no ar!!!!!!!")
+    })
+}).catch((error) => console.log("Erro ao conectar com o banco: "+error));
